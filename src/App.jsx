e44043c342352280a49d10d697e2afbead23933d
Alex <alex95.taylor@gmail.com>
2024-01-03 18:52:48 -0500

    Add logic for retrieving data from PokeAPi

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import CardList from './components/CardList'
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/';
+
+  //Array of Pokemon to be used in API request
+  const pokemonList = ['bulbasaur', 'charmander', 'squirtle', 'mewtwo', 'gloom', 'pikachu', 'pichu', 'plusle', 'minun', 'jigglypuff'];
+  const [cardDetails, setCardDetails] = useState([{id: '', name: '', img: ''}]);
+
+  const fetchData = (pokemon) => {
+    return axios.get(`${BASE_URL}${pokemon}`)
+      .then(response => response.data)
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  //Create array of unresolved promises from fetchData
+  const pokemonPromises = pokemonList.map(pokemon => fetchData(pokemon));
+  useEffect(() => {
+    //Waits for all the promises to resolve before sending the data to the callback
+    Promise.all(pokemonPromises)
+      .then(pokemonData => { 
+        processData(pokemonData); 
+      })
+      .catch(error => {
+        console.log(error);
+    });
+  },[]);
+
+  //Function for extracting only the needed data from the object
+  function processData(data) {
+    const newArray = [];
+
+    //Iterate over each item in the oject and push the chosen data into an array
+    data.forEach(pokemon => {
+      //Ensure the id always has 4 digits like an actual Pokedex entry
+      const id = pokemon.id.toString().padStart(4, '0');
+      const name = pokemon.name;
+      const img = pokemon.sprites.other.dream_world.front_default;
+
+      newArray.push({id: id, name: name, img: img});
+    });
+
+    //Update the state with the modified data
+    setCardDetails(newArray);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CardList cardDetails={cardDetails}/>
     </>
   )
 }
