09d8187ad2a76315418742dee6a1b696f162af69
Alex <alex95.taylor@gmail.com>
2024-01-04 14:12:51 -0500

    Add logic for clicking on the Card
    -Use the gridPosition prop as in the `order` property to set the Card
     position in the grid.
    -Send the proper id to the CardList component after click event.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,12 +5,19 @@ Card.propTypes = {
         id: PropTypes.string.isRequired,
         name: PropTypes.string.isRequired,
         img: PropTypes.string.isRequired
-    }).isRequired
+    }).isRequired,
+    gridPosition: PropTypes.number.isRequired,
+    onClick: PropTypes.func.isRequired
 };
 
-function Card({pokemon}) {
+function Card({pokemon, gridPosition, onClick}) {
+    const handleClick = () => {
+        //Send the pokemon id to the parent component
+        onClick(pokemon.id);
+    };
+
     return (
-        <div className="card">
+        <div className="card" style={{order: gridPosition}} onClick={handleClick}>
             <h2>#{pokemon.id}</h2>
             <h1>{pokemon.name}</h1>
             <img src={pokemon.img}/>
