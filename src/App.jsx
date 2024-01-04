f3f8c586107efea214825e49a2e138dfe86d8c35
Alex <alex95.taylor@gmail.com>
2024-01-04 14:55:59 -0500

    Add state variables & props for displaying scores

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import './card.css'
 import CardList from './components/CardList'
 import axios from 'axios';
+import Scoreboard from './components/Scoreboard';
 
 function App() {
   const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/';
@@ -10,6 +11,8 @@ function App() {
   //Array of Pokemon to be used in API request
   const pokemonList = ['bulbasaur', 'charmander', 'squirtle', 'mewtwo', 'gloom', 'pikachu', 'pichu', 'plusle', 'minun', 'jigglypuff'];
   const [cardDetails, setCardDetails] = useState([{id: '', name: '', img: ''}]);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
 
   const fetchData = (pokemon) => {
     return axios.get(`${BASE_URL}${pokemon}`)
@@ -52,7 +55,8 @@ function App() {
 
   return (
     <>
-      { cardDetails.length > 1 ? <CardList cardDetails={cardDetails}/> : null}
+      <Scoreboard currentScore={currentScore} bestScore={bestScore}/>
+      { cardDetails.length > 1 ? <CardList cardDetails={cardDetails} currentScore={currentScore} setCurrentScore={setCurrentScore} setBestScore={setBestScore}/> : null}
     </>
   )
 }
