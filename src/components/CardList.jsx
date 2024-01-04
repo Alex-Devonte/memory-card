10f187b1e8945dc5665916c1b030e943148ba207
Alex <alex95.taylor@gmail.com>
2024-01-04 14:08:40 -0500

    Add logic for populating & shuffling grid array
    -Grid array will be used to align card components in the grid
    -Return a Card component for each Pokemon in the cardDetails prop
    -Pass the individual Pokemon info, gridArray, and onClick function as
     props for the Card component.

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Card from "./Card";
 import PropTypes from 'prop-types'
 
@@ -12,12 +13,54 @@ CardList.propTypes = {
 };
 
 function CardList({cardDetails}) {
+    const [clickedCards, setClickedCards] = useState([]);
+
+    const populateGridArray = () => {
+        //create an array of numbers from 1 to the length of the cardDetails array
+        let gridArr = [];
+        for (let i = 1; i <= cardDetails.length; i++) {
+            gridArr.push(i);
+        }
+        return gridArr;
+    }
+
+    const shuffleGridArray = () => {
+        //Create shallow copy of original grid array
+        let shuffledArray = [...gridPositionArray];
+
+        //Loop through the array in reverse
+        for (let i = shuffledArray.length - 1; i > 0; i--) {
+            //Generate random number between 0 and i to be used for an index
+            let j = Math.floor(Math.random() * (i + 1));
+
+            //Swap elements at the specified indexes
+            let temp = shuffledArray[i];
+            shuffledArray[i] = shuffledArray[j];
+            shuffledArray[j] = temp;
+        }
+
+        return shuffledArray;
+    }
+
+    const handleClick = (id) => {
+        //End the game if user clicks on a repeat
+        //Otherwise add that id to the tracking array
+        if (clickedCards.includes(id)) {
+            alert('YOU LOSE');
+        } else {
+            setClickedCards([...clickedCards, id]); 
+        }
+    };
+    
+    const gridPositionArray = populateGridArray();
+    const newGridPositionArray = shuffleGridArray();
+
     return (
-        <>
+        <div id="card-list">
             {cardDetails.map((detail, key) => {
-                return <Card key={key} pokemon={detail}/>;
+                return <Card key={key} pokemon={detail} gridPosition={newGridPositionArray[key]} onClick={handleClick}/>;
             })}
-        </>
+        </div>
     )
 }
 
