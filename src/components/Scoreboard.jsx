35f598bd56ea416af55e8ae658643143e688310c
Alex <alex95.taylor@gmail.com>
2024-01-03 19:01:25 -0500

    Add initial scoreboard component

---
+++ src/components/Scoreboard.jsx
@@ -0,0 +1,10 @@
+function Scoreboard() {
+    return (
+       <div id="scoreboard">
+            <p>Current Score: </p>
+            <p>Best Score: </p>
+       </div>
+    )
+}
+
+export default Scoreboard;
