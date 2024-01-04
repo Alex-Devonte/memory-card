0dfb9ff8a0098ea884c0b62731d64292dda74183
Alex <alex95.taylor@gmail.com>
2024-01-04 14:53:55 -0500

    Add state variables &  props for displaying scores

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,8 +1,15 @@
-function Scoreboard() {
+import PropTypes from 'prop-types'
+
+Scoreboard.propTypes = {
+    currentScore: PropTypes.number.isRequired,
+    bestScore: PropTypes.number.isRequired,
+};
+
+function Scoreboard({currentScore, bestScore}) {
     return (
        <div id="scoreboard">
-            <p>Current Score: </p>
-            <p>Best Score: </p>
+            <p>Current Score: {currentScore}</p>
+            <p>Best Score: {bestScore}</p>
        </div>
     )
 }
